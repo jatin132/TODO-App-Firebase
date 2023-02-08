009aa4a5b3e714404f37615b60c36e15f96e53fe
Jatin Gupta <104434286+jatin132@users.noreply.github.com>
2023-02-08 14:10:43 +0530

    Application completed

---
+++ app/src/main/java/com/myapplication/to_dolistfirebase/OnDialogCloseListner.java
@@ -0,0 +1,7 @@
+package com.myapplication.to_dolistfirebase;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListner {
+    void onDialogClose(DialogInterface dialogInterface);
+}
